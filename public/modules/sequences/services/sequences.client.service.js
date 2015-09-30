758aee54ecad8a2ea63216942fe380c490d69e8d
rajasekaran247 <rajasekaran@247lifetime.com>
2015-09-30 16:38:56 +0530

    sequence generation module list navigation to expense type issue fixed

--- public/modules/sequences/services/sequences.client.service.js
+++ public/modules/sequences/services/sequences.client.service.js
@@ -3,7 +3,6 @@
 //Expense Types service used to communicate Sequences REST endpoints
 angular.module('sequences').factory('Sequences', ['$resource',
 	function($resource) {
-    console.log(':sequenceId');
 		return $resource('sequences/:sequenceId', { sequenceId: '@_id'
 		}, {
 			update: {
